c5444f539b67560e99ecc95c936dd96004fd99eb
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-10-12 16:33:46 +0200

    Added arbitrary html to the QuestionAdmin

--- django-tutorial/tutorial/polls/admin.py
+++ django-tutorial/tutorial/polls/admin.py
@@ -6,6 +6,6 @@ from polls.models import Question, Choice
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,               {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
 admin.site.register(Question, QuestionAdmin)
