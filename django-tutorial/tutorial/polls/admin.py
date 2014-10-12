e2aa0cc18deee4f64de1377d0756d73d11615ee9
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-10-12 16:29:26 +0200

    Customize the Question Admin in the admin site

--- django-tutorial/tutorial/polls/admin.py
+++ django-tutorial/tutorial/polls/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from polls.models import Question, Choice
 
-admin.site.register(Question)
-# Register your models here.
+#admin.site.register(Question)
+
+class QuestionAdmin(admin.Models):
+    fields = ['pub_date', 'question_text']
+
+admin.site.register(Question, QuestionAdmin)
