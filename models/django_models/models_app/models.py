030c67a408bc37c5887be4ebcb18f2afe01d57bd
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-10-18 21:16:32 +0200

    Added both musician and album models

--- models/django_models/models_app/models.py
+++ models/django_models/models_app/models.py
@@ -4,4 +4,13 @@ class Person(models.Model):
     first_name = models.CharField(max_length=254)
     last_name = models.CharField(max_length=254)
 
+class Musician(models.Model):
+    first_name = models.CharField(max_length=254)
+    last_name = models.CharField(max_length=254)
+    instrument = models.CharField(max_length=100)
 
+class Album(models.Model):
+    artist = models.ForeignKey(Musician)
+    name = models.CharField(max_length=254)
+    release_date = models.DateField()
+    num_stars = models.IntegerField()
