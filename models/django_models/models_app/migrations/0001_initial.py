8498a4c57efd223e0c91ec6055d460af639222fc
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-10-18 22:44:57 +0200

    Using the intermediate model for many to many fields

--- models/django_models/models_app/migrations/0001_initial.py
+++ models/django_models/models_app/migrations/0001_initial.py
@@ -11,14 +11,70 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
+            name='Album',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=254)),
+                ('release_date', models.DateField()),
+                ('num_stars', models.IntegerField()),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Musician',
+            fields=[
+                ('id_musician', models.AutoField(serialize=False, primary_key=True)),
+                ('first_name', models.CharField(max_length=254)),
+                ('last_name', models.CharField(max_length=254)),
+                ('instrument', models.CharField(max_length=100)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
             name='Person',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('first_name', models.CharField(max_length=254)),
                 ('last_name', models.CharField(max_length=254)),
+                ('shirt_size', models.CharField(max_length=1, choices=[(b'S', b'Small'), (b'M', b'Medium'), (b'L', b'Large')])),
             ],
             options={
             },
             bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Pizza',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Topping',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('type', models.CharField(max_length=254)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='pizza',
+            name='toppings',
+            field=models.ManyToManyField(to='models_app.Topping'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='album',
+            name='artist',
+            field=models.ForeignKey(to='models_app.Musician'),
+            preserve_default=True,
         ),
     ]
--- models/django_models/models_app/models.py
+++ models/django_models/models_app/models.py
@@ -11,7 +11,7 @@ class Person(models.Model):
     shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES)
 
 class Musician(models.Model):
-    id = models.AutoField(primary_key=True)
+    id_musician = models.AutoField(primary_key=True)
     first_name = models.CharField(max_length=254)
     last_name = models.CharField(max_length=254)
     instrument = models.CharField(max_length=100)
@@ -21,6 +21,19 @@ class Album(models.Model):
     name = models.CharField(max_length=254)
     release_date = models.DateField()
     num_stars = models.IntegerField()
+
+class Group(models.Model):
+    name = models.CharField(max_length=128)
+    members = models.ManyToManyField(Person, through='Membership')
+
+    def __unicode__(self):              
+        return self.name
+
+class Membership(models.Model):
+    person = models.ForeignKey(Person)
+    group = models.ForeignKey(Group)
+    date_joined = models.DateField()
+    invite_reason = models.CharField(max_length=64)
 
 
 class Topping(models.Model):
