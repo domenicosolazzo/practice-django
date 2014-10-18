e2e5194b634f9d7addd2fb9313794074784d67b8
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-10-18 21:29:23 +0200

    Added choices parameter

--- models/django_models/models_app/models.py
+++ models/django_models/models_app/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 
 class Person(models.Model):
+    SHIRT_SIZES = (
+        ('S', 'Small'),
+        ('M', 'Medium'),
+        ('L', 'Large'),
+    )
     first_name = models.CharField(max_length=254)
     last_name = models.CharField(max_length=254)
+    shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES)
 
 class Musician(models.Model):
     first_name = models.CharField(max_length=254)
