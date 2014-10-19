bedf006cb0484b3a8d26255b10107f1321c639ef
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-10-19 19:32:41 +0200

    Calling custom querysets

--- models/django_models/manager_app/models.py
+++ models/django_models/manager_app/models.py
@@ -5,6 +5,22 @@ class PeopleManager(models.Manager):
     def get_queryset(self):
         return super(PeopleManager, self).get_queryset().filter(first_name__startswith='Domenico')
 
+class PersonQuerySet(models.QuerySet):
+    def small_people(self):
+        return self.filter(shirt_size='S')
+
+    def medium_people(self):
+        return self.filter(shirt_size='M')
+
+class PersonManager(models.Manager):
+    def get_queryset(self):
+        return PersonQuerySet(self.model, using=self._db)
+
+    def small_people(self):
+        return self.get_queryset().small_people()
+
+    def medium_people(self):
+        return self.get_queryset().medium_people()
 
 class Person(models.Model):
     SHIRT_SIZES = (
@@ -17,5 +33,5 @@ class Person(models.Model):
     shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES)
 
     # Custom managers
-    people = models.Manager() # Person.people.all()
+    people = PersonManager() # Person.people.all()
     domenicos = PeopleManager() # Person.domenicos.all()
