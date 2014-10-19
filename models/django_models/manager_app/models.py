571fcb305c032b9842d435344f86befc2207313d
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-10-19 19:28:59 +0200

    Added new model and manager

--- models/django_models/manager_app/models.py
+++ models/django_models/manager_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+# Custom managers
+class PeopleManager(models.Manager):
+    def get_queryset(self):
+        return super(PeopleManager, self).get_queryset().filter(first_name__startswith='Domenico')
+
+
+class Person(models.Model):
+    SHIRT_SIZES = (
+        ('S', 'Small'),
+        ('M', 'Medium'),
+        ('L', 'Large'),
+    )
+    first_name = models.CharField(max_length=254)
+    last_name = models.CharField(max_length=254)
+    shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES)
+
+    # Custom managers
+    people = models.Manager() # Person.people.all()
+    domenicos = PeopleManager() # Person.domenicos.all()
