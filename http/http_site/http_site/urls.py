57969552ece6d87ad4b21140b0268f45144be02d
Domenico Solazzo <solazzo.domenico@gmail.com>
2014-10-19 20:15:59 +0200

    Added explanatexplanation for named groups

--- http/http_site/http_site/urls.py
+++ http/http_site/http_site/urls.py
@@ -1,10 +1,21 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from . import views
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'http_site.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^articles/2003/$', views.special_case_2003),
+    url(r'^articles/(?P<year>\d{4})/$', views.year_archive),
 
-    url(r'^admin/', include(admin.site.urls)),
+    #A request to /articles/2005/03/ would call
+    #the function views.month_archive(request, year='2005', month='03'),
+    #instead of views.month_archive(request, '2005', '03').
+    url(r'^articles/(?P<year>\d{4})/(?P<month>\d{2})/$', views.month_archive),
+    # A request to /articles/2003/03/03/ would call
+    # the function views.article_detail(request, year='2003', month='03', day='03')
+    url(r'^articles/(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/$', views.article_detail),
+
+    url(r'^admin/', include(admin.site.urls))
 )
