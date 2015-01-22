5b79b87d20434f65713f0b6e9f6ebb40fa5b60dd
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-01-22 18:03:43 +0100

    Imported Task model; Imported TaskSerializer, UserSerializer

--- task_api/scrum/board/views.py
+++ task_api/scrum/board/views.py
@@ -1,9 +1,21 @@
-from rest_framework import viewsets
+from rest_framework import authentication, permissions, viewsets
 
-from .models import Sprint
-from .serializers import SprintSerializer
+from .models import Sprint, Task
+from .serializers import SprintSerializer, TaskSerializer, UserSerializer
 
-class SprintViewSet(viewsets.ModelViewSet):
+class DefaultsMixin(object):
+	authentication_classes = (
+		authentication.BasicAuthentication,
+		authentication.TokenAuthentication
+	)
+	permission_classes = (
+		permissions.IsAuthenticated
+	)
+	paginate_by = 25
+	paginate_by_param = 'page_size'
+	max_paginate_by = 100
+
+class SprintViewSet(DefaultsMixin, viewsets.ModelViewSet):
 	"""API endpoint for listing and creating sprints"""
 
 	queryset = Sprint.objects.order_by('end')
