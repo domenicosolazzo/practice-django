cd9dac562c2f5ee3d2ae545d328f7ff8a9235545
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-01-22 18:07:35 +0100

    Added User and User viewsets

--- task_api/scrum/board/views.py
+++ task_api/scrum/board/views.py
@@ -3,6 +3,8 @@ from rest_framework import authentication, permissions, viewsets
 from .models import Sprint, Task
 from .serializers import SprintSerializer, TaskSerializer, UserSerializer
 
+User = get_user_model()
+
 class DefaultsMixin(object):
 	authentication_classes = (
 		authentication.BasicAuthentication,
@@ -20,3 +22,19 @@ class SprintViewSet(DefaultsMixin, viewsets.ModelViewSet):
 
 	queryset = Sprint.objects.order_by('end')
 	serializer_class = SprintSerializer
+
+class TaskViewSet(DefaultsMixin, viewsets.ModelViewSet):
+	"""API endpoint for listing and creating tasks"""
+
+	queryset = Task.objects.all()
+	serializer_class = TaskSerializer
+
+class UserViewSet(DefaultsMixin, viewsets.ReadOnlyModelViewSet):
+	"""API endpoint for listing users"""
+
+	lookup_field = User.USERNAME_FIELD
+	lookup_url_kwarg = User.USERNAME_FIELD
+	queryset = User.objects.order_by(User.USERNAME_FIELD)
+	serializer_class = UserSerializer
+
+	
