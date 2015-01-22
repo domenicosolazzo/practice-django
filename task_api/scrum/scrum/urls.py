d60d723777c39d7fa2337cd297c6cc65823a49f0
Domenico Solazzo <solazzo.domenico@gmail.com>
2015-01-22 18:18:51 +0100

    Added urls in the scrum app

--- task_api/scrum/scrum/urls.py
+++ task_api/scrum/scrum/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from rest_framework.authtoken.views import obtain_auth_token
 
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'scrum.views.home', name='home'),
@@ -9,4 +10,5 @@ urlpatterns = patterns('',
 
     #url(r'^admin/', include(admin.site.urls)),
     url(r'api/token', obtain_auth_token, name='api-token'),
+    url(r'api/', include(router.urls)),
 )
